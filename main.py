2d0169e370d53ea126c9dc35bde917a74b33bc07
mika1407 <koneautomaatio@hotmail.com>
2026-02-18 20:30:41 +0200

    mouse moves added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from checkers.game import Game
 from checkers.constants import WIDTH, HEIGHT, SQUARE_SIZE, RED
 from checkers.board import Board
 
@@ -7,27 +8,35 @@ FPS = 60
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Checkers")
 
+def get_row_col_from_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    col = x // SQUARE_SIZE
+    return row, col
 
 def main():
     run = True
     clock = pygame.time.Clock()
-    board = Board()
+    game=Game(WIN)
 
-    piece = board.get_piece(0,1)
-    board.move(piece, 4, 3)
 
     while run:
         clock.tick(FPS)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                pass
+                pos = pygame.mouse.get_pos()
+                row, col = get_row_col_from_mouse(pos)
+                game.select(row, col)
 
-        board.draw(WIN)
-        pygame.display.update()
+        game.update()
 
     pygame.quit()
 
